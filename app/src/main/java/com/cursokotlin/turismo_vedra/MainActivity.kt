abf609e16141df484ed9e7ded7a3be8dda8b6110
TatianaTrians <tatiana.obradoiro@gmail.com>
2022-07-01 14:34:55 +0200

    PaxinaPrincipal

--- app/src/main/java/com/cursokotlin/turismo_vedra/MainActivity.kt
+++ app/src/main/java/com/cursokotlin/turismo_vedra/MainActivity.kt
@@ -7,69 +7,40 @@ import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import com.cursokotlin.turismo_vedra.productoresLocales.ProductoresLocales
 
 class MainActivity : AppCompatActivity() {
-    protected lateinit var video: VideoView
-    protected lateinit var reproductorVideo: MediaPlayer
-    protected var mCurrentVideoPosition: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*video=findViewById(R.id.videoBenvidos)
-        val uri= Uri.parse("android.resource://"
-                + packageName
-                + "/"
-                + R.raw.videobenvidos)
-        video.keepScreenOn = true
-
-        video.setVideoURI(uri)
-        video.start()
-        video.setOnPreparedListener { mp ->
-            reproductorVideo=mp
-            reproductorVideo.isLooping=true
-
-            if (mCurrentVideoPosition !=0){
-                reproductorVideo.seekTo(mCurrentVideoPosition)
-                reproductorVideo.start()
-            }
-
-            //este cachito de abaixo é para mutear o video.
-            if (mp.isPlaying()) {
-                mp.stop()
-                mp.release()
-            }
-            mp.setVolume(0f, 0f)
+        var logoVedra=findViewById<ImageButton>(R.id.logoVedra)
+        logoVedra.setOnClickListener {
+            val acceder= Intent(this, MainActivity::class.java)
+            startActivity(acceder)
         }
 
+        var menu = findViewById<ImageButton>(R.id.menuHamburguesa)
+        menu.setOnClickListener {
+            val acceder = Intent(this, Menu::class.java)
+            startActivity(acceder)
+        }
 
-        /*var accesoProductores= findViewById<Button>(R.id.productores)
+        var accesoProductores= findViewById<Button>(R.id.productoresLocaisBoton)
         accesoProductores.setOnClickListener{
             val acceder = Intent(this, ProductoresLocales::class.java)
             startActivity(acceder)
-        }*/
-
-    }
-
-    override fun onPause() {
-        super.onPause()
-
-        mCurrentVideoPosition=reproductorVideo.currentPosition
-        video.pause()
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        video.start()
-    }*/
-        var menu=findViewById<ImageButton>(R.id.menuHamburguesa)
-        menu.setOnClickListener {
-            val acceder= Intent(this, MenuPrincipal::class.java)
-            startActivity(acceder)
+        }
+        var infoTuristica=findViewById<TextView>(R.id.tvInformacionTuristica)
+        infoTuristica.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.concellodevedra.es/es/node/459")))
+        }
+        var webConcello =findViewById<TextView>(R.id.tvWebVedra)
+        webConcello.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.concellodevedra.es/es")))
         }
     }
 }
