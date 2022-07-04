2db6cbd65d1802598d4bc7bff4bb088b9006c0e8
Alex <alexramosclase@gmail.com>
2022-07-04 15:20:51 +0200

    modificaciones

---
+++ app/src/main/java/com/cursokotlin/turismo_vedra/queVer/puenteColganteXimonde.kt
@@ -0,0 +1,12 @@
+package com.cursokotlin.turismo_vedra.queVer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cursokotlin.turismo_vedra.R
+
+class puenteColganteXimonde: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_puente_colgante_ximonde)
+    }
+}
