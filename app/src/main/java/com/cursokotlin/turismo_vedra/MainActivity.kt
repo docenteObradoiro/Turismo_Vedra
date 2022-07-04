4f5493289469c268545152ab8a648395afcbd48c
antiacasvaz <antiaobradoiro@gmail.com>
2022-07-04 09:04:48 +0200

    Bares e cafeterias

--- app/src/main/java/com/cursokotlin/turismo_vedra/MainActivity.kt
+++ app/src/main/java/com/cursokotlin/turismo_vedra/MainActivity.kt
@@ -3,7 +3,9 @@ package com.cursokotlin.turismo_vedra
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
+import com.cursokotlin.turismo_vedra.ondexantar.DondeComer_Restaurantes
 import com.cursokotlin.turismo_vedra.ondexantar.DondeComer_bares
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val botonbares = findViewById<ImageButton>(R.id.botonbares)
+        val botonbares = findViewById<Button>(R.id.botonbares)
 
         botonbares.setOnClickListener {
             val acceder = Intent(this, DondeComer_bares::class.java)
+            startActivity(acceder)
+        }
+
+        val botonrestaurantes = findViewById<Button>(R.id.botonrestaurantes)
+
+        botonrestaurantes.setOnClickListener {
+            val acceder = Intent(this, DondeComer_Restaurantes::class.java)
             startActivity(acceder)
         }
 
